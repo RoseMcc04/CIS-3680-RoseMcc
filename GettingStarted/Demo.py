89969aae7c1fa26caf7a7d79c92359bef039f06c
RoseMcc04 <mccormacka@appstate.edu>
2024-08-26 15:18:37 -0400

    started Python notes for class

---
+++ GettingStarted/Demo.py
@@ -0,0 +1,8 @@
+"""
+Author: Rose McCormack
+Version: 26 August 2024
+
+Description: This will be the first demonstration file for
+CIS 3680 - Programming Software Solutions. I hope to use this
+file to demonstrate data types and basic print statements. 
+"""
