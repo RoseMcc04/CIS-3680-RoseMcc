5d98bb8b10a79320696e6457cb2f4c295f23c9a7
RoseMcc04 <mccormacka@appstate.edu>
2024-08-28 15:52:32 -0400

    did basic inputs and printed a statement using string and int concatenation

--- GettingStarted/Demo.py
+++ GettingStarted/Demo.py
@@ -7,5 +7,9 @@ CIS 3680 - Programming Software Solutions. I hope to use this
 file to demonstrate data types and basic print statements. 
 """
 
-userInput = input("What is your name: ")
-print("Your name is: " + userInput)
+# Asking the user their name and age
+name = input("Please enter your name? ")
+age = int(input("How old are you? "))
+
+# Returns statement for the user from their inputs
+print("Your name is " + name + " and you are", age, "years old.")
